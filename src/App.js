fdb77b6fbe371186606587ee16479e275e63cb4e
sanjeeva-rao <sanjeevarao436@gmail.com>
2024-09-29 11:23:49 +0530

    Created Rect App COnfigured tailwind css

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="">
+      <h1 className='font-bold bg-red-300'>Namste React, Nextflix GPT!</h1>
+    </div>
+  );
+}
+
+export default App;
