118f9772f16cb7a96b291401673cc77bf9f136a2
sanjeeva-rao <sanjeevarao436@gmail.com>
2024-10-21 22:06:13 +0530

    -fetch Now Playing Movies from TMDB API -Store Movies in Redux store

--- src/components/Browser.js
+++ src/components/Browser.js
@@ -1,6 +1,23 @@
 import Header from "./Header";
 import Top from "./Top";
+import { tmdb_options } from "../utils/constants";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { addNowPlayingMovies } from "../redux/moviesSlice";
+
 const Browser = () => {
+    const dispatch = useDispatch();
+    const getNowPlayingMovies = async()=>{
+        const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', tmdb_options);
+        const json = await data.json();
+        console.log(json.results, "json");
+        dispatch(addNowPlayingMovies(json.results));
+    }
+
+    useEffect(()=>{
+        getNowPlayingMovies();
+    }, [])
+
     return <div>
         <Top/>
         <Header />
